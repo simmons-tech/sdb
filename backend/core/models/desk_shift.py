7ad2f9f53f8dd385a6481116597214010f370e1a
Samantha York <samimak@mit.edu>
2020-08-11 16:43:59 -0800

    Modified DeskShift so start/end is required

--- backend/core/models/desk_shift.py
+++ backend/core/models/desk_shift.py
@@ -8,6 +8,6 @@ class DeskShift(models.Model):
     A model that keeps track of the schedule of the simmons desk workers
     """
 
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField(blank=False)
+    end_time = models.DateTimeField(blank=False)
     desk_worker = models.ForeignKey(User, on_delete=models.CASCADE)
