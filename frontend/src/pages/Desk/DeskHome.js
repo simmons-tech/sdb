5d080d6ada93b8dc22ebf5d9fe81048be30edd7a
Jerry Zhang <jzhang56@mit.edu>
2020-08-13 13:00:03 -0500

    visual change to dashboard

--- frontend/src/pages/Desk/DeskHome.js
+++ frontend/src/pages/Desk/DeskHome.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import BasePage from "../BasePage";
 import { Jumbotron, Row, Col } from "reactstrap";
 import UserTable from "../../components/StripedTable";
-import NotesComponent from "./NotesComponent";
-import AddNotes from "./AddNotes";
+import NotesComponent from "./desk_components/NotesComponent";
+import AddNotes from "./desk_components/AddNotes";
 import axios from "../../axiosInstance";
 
 class DeskHome extends Component {
     constructor(props) {
         super(props);
-        this.state = { loading: false, rows: [], notes: [], announcements: [] };
+        this.state = { loading: false, rows: [], notes: []};
     }
 
     // async componentDidMount() {
@@ -29,31 +29,28 @@ class DeskHome extends Component {
     render() {
         return (
             <BasePage loading={this.state.loading} header="Dashboard">
-                {/* <UserTable
-                   rows={this.state.rows}
-                   headers={["Location", "Recipient"]}
-                /> */}
+                
                 <Jumbotron>
                     Test Dashboard
                 </Jumbotron>
 
                 <Row>
-                    <Col>
+                    <Col xs="4">
                         <Jumbotron>
                             Notes
                             <AddNotes/>
-                            <p></p> {/* used for the extra space. could find a better way to do this. */}
+                            <p></p> {/* used for the extra space. */}
                             <NotesComponent notes={this.state.notes}/>
                         </Jumbotron>                   
                     </Col>
                     <Col>
                         <Jumbotron>
-                            Something
-                        </Jumbotron> 
-                    </Col>
-                    <Col>
-                        <Jumbotron>
-                            Something Else
+                            Checked out items
+                            <p></p>
+                            <UserTable
+                                rows={this.state.rows}
+                                headers={["Name", "Item", "Return Date"]}
+                            /> 
                         </Jumbotron> 
                     </Col>
                 </Row>
