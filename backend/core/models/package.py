6172b2724cde3eb9b3d09cbd5ec7769ced13e49c
Samantha York <samimak@mit.edu>
2020-08-14 20:44:23 -0800

    Attached `AllPackages.js` to backend
    Also fixed a query bug in `package.py` where the filter was trying to
    sort a datetime field by a Boolean Value

--- backend/core/models/package.py
+++ backend/core/models/package.py
@@ -11,7 +11,7 @@ class WaitingPackages(models.Manager):
     """
 
     def get_queryset(self):
-        return super().get_queryset().filter(picked_up=True)
+        return super().get_queryset().filter(picked_up__isnull=True)
 
 
 class Package(models.Model):
