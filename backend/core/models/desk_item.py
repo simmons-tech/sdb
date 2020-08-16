539b255d9ab0a895b3f66376221b4286a866c8de
Samantha York <samimak@mit.edu>
2020-08-16 13:38:31 -0800

    Added ModelManagers to DeskItem

--- backend/core/models/desk_item.py
+++ backend/core/models/desk_item.py
@@ -3,6 +3,24 @@ from django.db import models
 from .user import User
 
 
+class CheckedOutItems(models.Manager):
+    """
+    A queryset that operates on all of the desk items that are currently checked out by residents
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(checked_out=False)
+
+
+class AvailableItems(models.Manager):
+    """
+    A queryset that operates on all of the desk items that are available to be checked out
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(checked_out=True)
+
+
 class DeskItem(models.Model):
     """
     Defines an object that can be checked out from the Simmons front desk
@@ -14,3 +32,7 @@ class DeskItem(models.Model):
     checked_out = models.BooleanField(default=False)
     resident = models.ForeignKey(User, on_delete=models.CASCADE, related_name="item_loaned")
     desk_worker = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    available_objects = AvailableItems()
+    checked_out_objects = CheckedOutItems()
+    objects = models.Manager()
