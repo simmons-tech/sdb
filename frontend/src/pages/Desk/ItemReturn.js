48a5ec1a591a00c9bfb4cf50f1c75e0331325893
Jerry Zhang <extramilemore@gmail.com>
2020-09-01 13:18:57 -0500

    implemented item return

--- frontend/src/pages/Desk/ItemReturn.js
+++ frontend/src/pages/Desk/ItemReturn.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import BasePage from "../BasePage";
-import {Jumbotron, Row, Col} from 'reactstrap';
+import {Jumbotron, Row, Col, Button} from 'reactstrap';
 import axios from "../../axiosInstance";
 import UserSearch from "./desk_components/UserSearch";
 import InteractiveUserTable from "./desk_components/InteractiveUserTable";
@@ -8,7 +8,17 @@ import InteractiveUserTable from "./desk_components/InteractiveUserTable";
 class ItemReturn extends Component {
     constructor(props) {
         super(props);
-        this.state = { loading: false, searched: false, users: [], loans:[]};
+        this.state = { 
+            loading: false, 
+            searched: false, 
+            users: [], 
+            loans:[], 
+            pks:[],
+            pk:false,
+            current_user: "",
+            current_item: "",
+            confirm: false,
+        };
     }
 
     onUserQuery = (values, callback) => {
@@ -26,7 +36,10 @@ class ItemReturn extends Component {
                     ]
                 ),
                 searched_user: true,
-                selected_user: false,
+                current_user: "",
+                current_item: "",
+                confirm: false
+
             });
         });
         callback();
@@ -40,13 +53,57 @@ class ItemReturn extends Component {
         .then(res => {
             console.log(res)
             this.setState({
+                current_user : row[1] + " " + row[0],
                 loans: res.data.map(loan => 
                     [
-
+                        loan.item.item,
+                        loan.num_checked_out,
+                        new Date(loan.time_due).toLocaleDateString(
+                            undefined, 
+                            {
+                                year: 'numeric', 
+                                month: 'long', 
+                                day: 'numeric'
+                            }) 
+                    ]
+                ),
+                pks: res.data.map(loan => 
+                    [
+                        loan.pk
                     ]
                 )
             })
         })
+    }
+
+    handleItemSelect = (row) => {
+        console.log(row)
+        let pk_index = this.state.loans.findIndex(e => e === row);
+        console.log(pk_index)
+        console.log(this.state.pks)
+
+        this.setState({
+            current_item: row[0],
+            confirm: true,
+            pk: this.state.pks[pk_index]
+        })
+
+    }
+
+    handleReturn = () => {
+
+        axios.post("/api/loans/" + this.state.pk + "/ret/",{})
+            .then(res =>{
+                this.setState({
+                    loans:[], 
+                    pks:[],
+                    pk:false,
+                    current_user: "",
+                    confirm: false,
+                })
+        })
+
+
     }
 
 
@@ -60,18 +117,36 @@ class ItemReturn extends Component {
                         {(this.state.searched_user) ? 
                             <Col>
                                 <Jumbotron>
-                                    {(this.state.users.length)?
-                                            <div>
-                                                <h4>Select a User</h4>
+                                {(this.state.current_user.length) ? 
+                                        <div>
+                                            <h4>Select an Item to return</h4>
                                                 <InteractiveUserTable
-                                                    rows={this.state.users}
-                                                    headers={["Last", "First", "Title", "Username", "Room", "Year"]}
-                                                    handleOnClick = {this.handleUserSelect}
+                                                    rows={this.state.loans}
+                                                    headers={["Name", "Quantity", "Date Due"]}
+                                                    handleOnClick = {this.handleItemSelect}
                                                 />
-                                            </div>   
-                                        :
-                                           <h4>There are no users that match your query</h4> 
-                                    }
+                                            {(this.state.confirm) ? 
+                                                <Button onClick = {this.handleReturn} color="primary">Return {this.state.current_item}</Button>
+                                            :
+                                                <div></div>
+                                            }
+                                        </div>
+                                        
+                                    :
+                                        (this.state.users.length)?
+                                            
+                                        <div>
+                                            <h4>Select a User</h4>
+                                            <InteractiveUserTable
+                                                rows={this.state.users}
+                                                headers={["Last", "First", "Title", "Username", "Room", "Year"]}
+                                                handleOnClick = {this.handleUserSelect}
+                                            />
+                                        </div>   
+                                    :
+                                    <h4>There are no users that match your query</h4> 
+                            
+                                }
                                 </Jumbotron>
                             </Col>
                         :
