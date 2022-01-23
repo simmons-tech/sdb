82cd269532e034c60ccbae442687cd42eac3cb77
Jordan Wilke <wilke18@mit.edu>
2022-01-23 17:28:15 -0500

    Finished merge

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -34,17 +34,11 @@ router.register(r'officers', views.Officers, 'officers')
 router.register(r'rooms', views.RoomList, 'rooms')
 router.register(r'sections', views.SectionList, 'sections')
 router.register(r'accounts', views.AccountsList, 'accounts')
-<<<<<<< HEAD
-router.register(r'meetings', views.MeetingList, 'meetings')
-router.register(r'proposals', views.ProposalList, 'proposals')
-
-=======
 router.register(r'packages', views.Packages, 'packages')
 router.register(r'desknotes', views.DeskNotes, 'desknotes')
 router.register(r'deskshifts', views.DeskShifts, 'deskshifts')
 router.register(r'deskitems', views.DeskItems, 'deskitems')
 router.register(r'loans', views.ItemLoans, 'loans')
->>>>>>> master
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -3,11 +3,7 @@ from .groups import *
 from .room import *
 from .section import *
 from .finances import *
-<<<<<<< HEAD
-from .meetings import *
-=======
 from .desk_item import *
 from .desk_notes import *
 from .desk_shift import *
 from .package import *
->>>>>>> master
